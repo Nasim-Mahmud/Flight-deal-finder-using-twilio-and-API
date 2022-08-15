91e0b51446e5d6a74e0bc4b40ab235002b4acc01
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-08-15 20:23:50 +0600

    Adding sheety

--- main.py
+++ main.py
@@ -3,9 +3,14 @@
 
 # Adding parameters
 import requests
+
+sheety_headers = {
+"Authorization": "Basic TmFzaW06MTk5NjIyODA="
+}
 sheety_parameters = {
-    "Authorization": "Basic TmFzaW06MTk5NjIyODA="
+
 }
 
 sheety_response = requests.get(url="https://api.sheety.co/43bd6cbcf5d90c6b7ffb3c6b3c961f51/myFlightDeals/prices",
-                        params=sheety_parameters)
+                        params=sheety_parameters, headers=sheety_headers)
+print(sheety_response.text)
