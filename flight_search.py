bc648201a4d7ab83e0c9b7c3d24b48293373ecce
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-08-16 22:47:41 +0600

    Updating

--- flight_search.py
+++ flight_search.py
@@ -1,3 +1,9 @@
 class FlightSearch:
     #This class is responsible for talking to the Flight Search API.
-    pass
+
+    def __init__(self):
+        pass
+
+    def destination_code(self):
+        code = "TESTING"
+        return code
