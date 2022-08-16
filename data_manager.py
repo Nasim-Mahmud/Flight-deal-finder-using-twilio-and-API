8c911aea54d626a3dee6a8d92bdcf4d45a3e2f6b
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-08-16 12:29:31 +0600

    Setting up data manager

--- data_manager.py
+++ data_manager.py
@@ -2,8 +2,9 @@ import requests
 
 SHEETY_ENDPOINT = "https://api.sheety.co/43bd6cbcf5d90c6b7ffb3c6b3c961f51/myFlightDeals/prices"
 
+
 class DataManager:
-    #This class is responsible for talking to the Google Sheet.
+    # This class is responsible for talking to the Google Sheet.
     def __init__(self):
         self.destination = {}
 
@@ -11,3 +12,5 @@ class DataManager:
         sheety_response = requests.get(url=SHEETY_ENDPOINT)
         sheet_data = sheety_response.json()
         self.destination = sheet_data["prices"]
+
+        return self.destination
