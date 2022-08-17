123c7c18daa33b355c109cacbf136ce6aa242917
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-08-17 14:56:32 +0600

    Updating

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes
 # to achieve the program requirements.
+from data_manager import DataManager
+from flight_search import FlightSearch
 
 # Adding parameters
 
