46c0a6bddf19e951a5ccd9b2ba3bb367a669d81e
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-08-20 20:34:27 +0600

    Updating

--- data_manager.py
+++ data_manager.py
@@ -2,7 +2,6 @@ import requests
 
 SHEETY_ENDPOINT = "https://api.sheety.co/43bd6cbcf5d90c6b7ffb3c6b3c961f51/myFlightDeals/prices"
 
-
 class DataManager:
     # This class is responsible for talking to the Google Sheet.
     def __init__(self):
