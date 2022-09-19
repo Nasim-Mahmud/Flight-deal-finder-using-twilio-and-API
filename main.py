52ddb1ee48af10b61fc86f882713e2a8b736424e
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-09-20 04:30:33 +0600

    Initializing

--- main.py
+++ main.py
@@ -6,11 +6,13 @@ import requests
 shetty_endpoint = "https://api.sheety.co/43bd6cbcf5d90c6b7ffb3c6b3c961f51/fd/prices"
 tequila_endpoint = "https://api.tequila.kiwi.com/v2/search"
 
-tequila_apiKey = "mwtoLjx_0-VcKUyqwJBgOFr2omnjP88_"
-
-teq_parameters = {
-    "fly_from": "Dhaka"
+tequila_apiKey_header = {
+    "apikey": "mwtoLjx_0-VcKUyqwJBgOFr2omnjP88_"
 }
 
-response = requests.get(url=tequila_endpoint, params=teq_parameters, headers=tequila_apiKey)
+teq_parameters = {
+    "fly_from": "FRA"
+}
+
+response = requests.get(url=tequila_endpoint, params=teq_parameters, headers=tequila_apiKey_header)
 print(response.text)
