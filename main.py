33ed50020d1ec47424df97b0b2429223bc586b1c
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-09-20 04:12:44 +0600

    Initializing

--- main.py
+++ main.py
@@ -1,2 +1,4 @@
 # This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager
 # classes to achieve the program requirements.
+
+shetty_endpoint = "https://api.sheety.co/43bd6cbcf5d90c6b7ffb3c6b3c961f51/fd/prices"
