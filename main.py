0d00415e96f2f6aa5c7565d39d8e0145e1bcd6eb
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-09-20 21:42:25 +0600

    Putting the IATA codes into the Google sheets using tequila and sheety api

--- main.py
+++ main.py
@@ -31,5 +31,6 @@ for i in range(0, len(sheety_data["prices"])):
         }
     }
 
+
     sheety_put_endpoint = f"{sheety_endpoint}/{i + 2}"
     IATA_sheety = requests.put(url=sheety_put_endpoint, json=sheety_parameters)
