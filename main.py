9f4b469431b2711849c917c00a7e038a9c095c08
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-09-22 01:04:04 +0600

    Searching for the cheapest flight!

--- main.py
+++ main.py
@@ -67,4 +67,5 @@ for i in range(0, len(sheety_data["prices"])):
         if teq_data['data'][j]['price'] <= price:
             pp.pprint(f"Destination {city_names}, Date: {target_date}, fare: {teq_data['data'][j]['price']}")
         else:
-            print(f"In {city_names}, Expected price {price}, No budget flight on {target_date}. Actual fare is: {teq_data['data'][j]['price']} USD")
+            print(
+                f"In {city_names}, Expected price {price}, No budget flight on {target_date}. Actual fare is: {teq_data['data'][j]['price']} USD")
