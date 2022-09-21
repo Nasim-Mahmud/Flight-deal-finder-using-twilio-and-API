047192d4107d964447e4e9a291d64508a58c70e0
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-09-21 21:29:33 +0600

    Searching for the cheapest flight!

--- main.py
+++ main.py
@@ -14,24 +14,25 @@ sheety_response = requests.get(url=sheety_endpoint)
 sheety_data = sheety_response.json()
 
 # Putting the IATA codes into the Google sheets using tequila and sheety api
-for i in range(0, len(sheety_data["prices"])):
-    city_names = sheety_data["prices"][i]["city"]
-    prices = sheety_data["prices"][i]["lowestPrice"]
-    teq_parameters = {
-        "term": city_names,
-    }
-
-    teq_response = requests.get(url=tequila_endpoint, params=teq_parameters, headers=tequila_apiKey_header)
-    teq_data = teq_response.json()
-    IATA_codes = teq_data["locations"][0]["code"]
-
-    sheety_parameters = {
-        "price": {
-            "iataCode": IATA_codes,
-        }
-    }
-
-    sheety_put_endpoint = f"{sheety_endpoint}/{i + 2}"
-    IATA_sheety = requests.put(url=sheety_put_endpoint, json=sheety_parameters)
+# for i in range(0, len(sheety_data["prices"])):
+#     city_names = sheety_data["prices"][i]["city"]
+#     prices = sheety_data["prices"][i]["lowestPrice"]
+#     teq_parameters = {
+#         "term": city_names,
+#     }
+#
+#     teq_response = requests.get(url=tequila_endpoint, params=teq_parameters, headers=tequila_apiKey_header)
+#     teq_data = teq_response.json()
+#     IATA_codes = teq_data["locations"][0]["code"]
+#
+#     sheety_parameters = {
+#         "price": {
+#             "iataCode": IATA_codes,
+#         }
+#     }
+#
+#     sheety_put_endpoint = f"{sheety_endpoint}/{i + 2}"
+#     IATA_sheety = requests.put(url=sheety_put_endpoint, json=sheety_parameters)
 
 #     Cheapest flight search
+
